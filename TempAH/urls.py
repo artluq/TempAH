189b0d5ba7119e0ef978c2a59aeba45ed45c3147
Luqman Razak <luqy@Luqmans-MacBook-Pro.local>
2024-10-25 02:36:36 +0800

    code on urls.py

--- TempAH/urls.py
+++ TempAH/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('services/', views.service_list, name='service_list'),
+    path('services/<int:service_id>/book/', views.book_service, name='book_service'),
+    path('booking/<int:booking_id>/confirmation/', views.booking_confirmation, name='booking_confirmation'),
 ]
+
