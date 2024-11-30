ae7adb731f967c1350e4ccc098ecee1bccc6470e
Luqman Razak <artluq11@gmail.com>
2024-12-01 04:09:04 +0800

    deleted:    ai.py 	deleted:    backend_old/__init__.py 	deleted:    backend_old/appointments/__init__.py 	deleted:    backend_old/appointments/admin.py 	deleted:    backend_old/appointments/apps.py 	deleted:    backend_old/appointments/migrations/0001_initial.py 	deleted:    backend_old/appointments/migrations/__init__.py 	deleted:    backend_old/appointments/models.py 	deleted:    backend_old/appointments/serializers.py 	deleted:    backend_old/appointments/tests.py 	deleted:    backend_old/appointments/urls.py 	deleted:    backend_old/appointments/views.py 	deleted:    backend_old/manage.py

--- backend/API/API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ backend/API/API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5fc8ec748a5126c3e42e7a60476f45ef41d5e8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10f85bba5bbbf86d2abb0a8f895c3b73d3c16689")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
