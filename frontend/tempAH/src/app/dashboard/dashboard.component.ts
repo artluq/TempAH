de3f4e7751100df06dee6ca6c3b573304ece3724
alepazahar <aliffazahar94@gmail.com>
2024-12-08 10:27:55 +0800

    Logout button at dashboard

--- frontend/tempAH/src/app/dashboard/dashboard.component.ts
+++ frontend/tempAH/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../service/api.service';
 import { ViewBooking } from '../model/appointment.model';
 import { Route, Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,7 +15,7 @@ export class DashboardComponent implements OnInit {
   upcomingAppointments: ViewBooking[] = []; // Updated to store ViewBooking objects
   
 
-  constructor(private bookingService: ApiService, private router: Router) {}
+  constructor(private bookingService: ApiService, private router: Router, private authService: AuthService,) {}
 
   ngOnInit() {
     this.username = sessionStorage.getItem('username') || 'User'; 
@@ -39,5 +40,14 @@ export class DashboardComponent implements OnInit {
   viewUpcomingAppointments() {
     // Logic to view detailed upcoming appointments
     // this.router.navigate(['/appointments']);
+  }
+
+  logout() {
+    const confirmed = window.confirm("Are you sure you want to log out?");
+    if (confirmed) {
+      this.authService.logout();  // Use the AuthService to log out
+      alert("Logged out successfully");
+      this.router.navigate(['/']);
+    }
   }
 }
